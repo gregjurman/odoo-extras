c1fa9d64b6beb6dc66a642e1ffe50e845f0e1edd
Greg Jurman <jurman.greg@gmail.com>
2015-02-23 18:57:09 -0500

    Fix missing import

--- sale_require_docs/sale.py
+++ sale_require_docs/sale.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 class sale_order(osv.osv):
     _inherit = "sale.order"
