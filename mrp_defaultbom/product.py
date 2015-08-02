d0d5f4ef019a5ecfa82dd097ecfdddae9700fa29
gregjurman <gregjurman@users.noreply.github.com>
2015-08-02 04:37:20 +0000

    Remove extranous PDB trace

--- mrp_defaultbom/product.py
+++ mrp_defaultbom/product.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
-import pdb
 
 class product_template(osv.osv):
     _inherit = "product.template"
@@ -9,7 +8,7 @@ class product_template(osv.osv):
     def create(self, cr, uid, vals, context=None):
         product_template_id = super(product_template, self).create(cr, uid, vals, context=context)
         mrp_bom = self.pool.get("mrp.bom")
-#        pdb.set_trace()
+
         data = {
             'name': "%s - Default BoM" % vals['name'],
             'product_tmpl_id': product_template_id,
